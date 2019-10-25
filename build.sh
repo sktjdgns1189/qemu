aa09e93e13707b2c6fc1136726171e2ebe66901c
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2019-10-25 23:22:23 +0200

    WIP: TEEGRIS AFL persistent mode

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+#export PATH=/home/alexander/builds/emu/afl-unicorn/:$PATH
+export PATH=~/Documents/workspace/bin/llvm/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04/bin/:$PATH
+export AFL_PATH=/home/alexander/builds/emu/afl-unicorn/
+
 ./configure \
+	--cc=/home/alexander/builds/emu/afl-unicorn/afl-clang-fast \
+	--host-cc=/home/alexander/builds/emu/afl-unicorn/afl-clang-fast \
 	--target-list=arm-linux-user,aarch64-linux-user \
 	--enable-user \
 	--disable-system \
