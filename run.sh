70ad2c084f303e96604cf414f6a2082ecfacc189
SungHun Na <nsh1189@gmail.com>
2020-01-05 17:11:03 +0900

    run.sh: change sboot_bl2 path

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./aarch64-softmmu/qemu-system-aarch64 -m 2048 -M virt -serial stdio -bios ~/Downloads/sw/pda/s10/BL/sboot_bl2.bin   -s -S 2>/dev/null
+./aarch64-softmmu/qemu-system-aarch64 -m 2048 -M virt -serial stdio -bios ../sboot_bl2.bin   -s -S 2>/dev/null
